450d1da64602d31e29e6dd8ea8940098768776c0
badeBhai <prakhardevs1@gmail.com>
2022-07-03 00:15:10 +0530

    Google Clone app build env

--- pages/search.js
+++ pages/search.js
@@ -34,7 +34,7 @@ export async function getServerSideProps(context){
     const useDummyData = false;
     const startIndex = context.query.start || '0';
 
-    const data = useDummyData ? Response: await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json());
+    const data = useDummyData ? Response: await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json());
 
     //After the SERVER has rendered...  Pass the result to the client
 
